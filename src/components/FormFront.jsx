48973e5ca3bf00c170e01086944995cdc72b73a9
Lucas Sant <lucasfdesantana095@gmail.com>
2026-04-16 11:57:27 -0300

    feat: create function for api

--- src/components/FormFront.jsx
+++ src/components/FormFront.jsx
@@ -1,22 +1,66 @@
+import { useState } from 'react';
+import api from '../service/api';
+
 function FormFront() {
+
+    const [nome, setNome] = useState("");
+    const [sobrenome, setSobrenome] = useState("");
+    const [cpf, setCpf] = useState("");
+
+    async function cadastrarAluno(e) {
+        e.preventDefault();
+        try {
+            const response = await api.post("/usuarios", {
+                nome,
+                sobrenome,
+                cpf
+            });
+
+            console.log("Usuário criado:", response.data)
+
+            setNome("");
+            setSobrenome("");
+            setCpf("");
+        } catch (error) {
+            console.error("Erro ao cadastrar:", error)
+        }
+    }
+
     return (
         <>
             <main>
-                <form action="">
+                <form onSubmit={cadastrarAluno}>
                     <div>
                         <label htmlFor="nome">Nome</label>
-                        <input type="text" placeholder="nome" name="nome" />
+                        <input
+                            type="text"
+                            placeholder="nome"
+                            name="nome"
+                            value={nome}
+                            onChange={(e) => setNome(e.target.value)} />
                     </div>
                     <div>
                         <label htmlFor="sobrenome">Sobrenome</label>
-                        <input type="text" placeholder="sobrenome" name="sobrenome" />
+                        <input
+                            type="text"
+                            placeholder="sobrenome"
+                            name="sobrenome"
+                            value={sobrenome}
+                            onChange={(e) => setSobrenome(e.target.value)}/>
                     </div>
                     <div>
                         <label htmlFor="cpf">cpf</label>
-                        <input type="text" placeholder="cpf" name="cpf" />
+                        <input
+                            type="text"
+                            placeholder="cpf"
+                            name="cpf"
+                            value={cpf}
+                            onChange={(e) => setCpf(e.target.value)}/>
                     </div>
 
-                    <button type="submit">Cadastrar Aluno</button>
+                    <button type="submit">
+                        Cadastrar Aluno
+                    </button>
                 </form>
             </main>
         </>
